25302427e02add86c66114d58e688771033624be
Jonathan Sarry <jsarry@gmail.com>
2024-03-30 17:24:36 +0000

    dd styles and fonts

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: ["@nuxtjs/tailwindcss", '@nuxtjs/color-mode', "@nuxt/fonts"],
+  colorMode: {
+    preference: 'synthwave', // default theme
+    dataValue: 'theme', // activate data-theme in <html> tag
+    classSuffix: '',
+  },
 })
--- web/pages/login.vue
+++ web/pages/login.vue
@@ -1,11 +1,19 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    <h1>Hello User</h1>
-    <p>What's your name?</p>
-    <input type="text">
+  <div class="flex-grow flex items-center justify-center">
+    <div class="login-element">
+      <span class="text-9xl">Hello </span> <span class="text-9xl font-mono font-bold"> USER</span>
+      <h2 class="pt-4">What's your name?</h2>
+      <input type="text" class="input input-bordered w-full input-accent">
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+div {
+  font-family: "Oswald", sans-serif;
+}
+
+</style>
