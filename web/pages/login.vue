5ebf4584a93cd2d0a0b5d246ee7045e788e19c7a
Jonathan Sarry <jsarry@gmail.com>
2024-03-30 13:07:28 +0000

    Add styling and pocketbase

---
+++ web/pages/login.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>Hello User</h1>
+    <p>What's your name?</p>
+    <input type="text">
+  </div>
+</template>
+
+<style scoped></style>
