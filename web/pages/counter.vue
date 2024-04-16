3f79ad55e43e95f60d478ea809b6b9dbf6103bca
Jonathan Sarry <jsarry@gmail.com>
2024-04-16 00:25:23 +0000

    add styles

--- web/pages/counter.vue
+++ web/pages/counter.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ref, onMounted  } from 'vue';
+import { ref, onMounted } from 'vue';
 import { UserCollection, CounterCollection } from '../pocketbase/client';
 import type { CountsResponse, UsersResponse } from '../server/types/pocketbase-types';
 
@@ -46,23 +46,26 @@ onMounted(async () => {
 
 const onClick = async () => {
   const player = counters.value.find(p => p.user_id === availableUser.value.id);
-  await CounterCollection.update(availableUser.value.countRecord?.id!, { value: player!.value + 1});
+  await CounterCollection.update(availableUser.value.countRecord?.id!, { value: player!.value + 1 });
 }
 
 </script>
 
 <template>
-  <div class="self-center min-w-36">
-    <div class="player-list flex flex-row min-h-96 max-h-96 bg-gray-500">
-      <ul>
-        <li v-for="user in counters" :key="user.id">
-          {{ user.expand?.name }} - {{ user.value }}
-        </li>
-      </ul>
+  <div class="self-center min-w-96">
+    <div class="player-list flex card flex-row min-h-96 max-h-96  overflow-auto bg-white shadow-xl">
+      <table class="table">
+        <tbody class="text-slate-500">
+          <tr v-for="user in counters" :key="user.id">
+            <td>{{ user.expand?.name }}</td>
+            <td>{{ user.value }}</td>
+          </tr>
+        </tbody>
+      </table>
 
 
     </div>
-    <div class="action-area">
+    <div class="action-area mt-4">
       <button @click="onClick" class="btn btn-primary">Click!</button>
     </div>
   </div>
